0e7caa8b54213a63190093f51a47723b53968774
Rudy Neeser <rudy.neeser@brandseye.com>
2014-06-30 10:26:17 +0200

    bug fix: fixed a bug in which some labels may be rotated, but not others

--- src/brandseye.js
+++ src/brandseye.js
@@ -81,6 +81,7 @@ var brandseye = {
     // ### Changes
     // We version the library using [semantic versioning](http://semver.org/).
     //
+    // - 1.2.2: Fixed a bug in which some labels may be rotated, but not others
     // - 1.2.1: Fixed a bug in which the support for hours broke the highlighting of the beginning of the month
     //          in the histogram.
     // - 1.2.0: Histogram now supports an "hourly" coarseness.
@@ -1162,9 +1163,10 @@ brandseye.charts = function() {
                 }
 
                 /* TODO sometimes we should rotate nothing, if the rangeBand is large enough. */
+                var mondayRotate = that.coarseness() != 'hourly' && isMonday(m);
                 var rangeBandWidth = that.coarseness() == 'hourly' ? 30 : 20;
                 var rotate = xScale.rangeBand() / that.data().length < rangeBandWidth ||
-                    that.longFormat(m, position) || isMonday(m) || isDayStart(m) ||
+                    that.longFormat(m, position) || mondayRotate || isDayStart(m) ||
                     _(that.data()).any(function(d) { return d.values[0].published === data;});
                 var angle = that.coarseness() === 'monthly' || that.coarseness() === 'weekly' ? -30 : -90;
                 var yOffset = that.coarseness() === 'monthly' || that.coarseness() === 'weekly' ? 0 : 5;
