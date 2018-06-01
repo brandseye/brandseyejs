b5758edff8993ce28d12159e24543d5d83aa4a8f
Constance Neeser <connie@brandseye.com>
2018-06-01 14:37:28 +0200

    Now handles data being reloaded

--- src/ColumnChart.js
+++ src/ColumnChart.js
@@ -54,23 +54,29 @@ export class ColumnChart {
     height = this._height - margin.top - margin.bottom;
 
     // set the ranges
-    var x = d3.scaleBand()
+    let x = d3.scaleBand()
               .range([0, width])
               .padding(0.02);
-    var y = d3.scaleLinear()
+    let y = d3.scaleLinear()
               .range([height, 0]);
+
+    let test = d3.select(this._element).select("svg");
+
 
     // append the svg object to the body of the page
     // append a 'group' element to 'svg'
     // moves the 'group' element to the top left margin
-    var svg = d3
-      .select(this._element)
-      .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform",
-              "translate(" + margin.left + "," + margin.top + ")");
+    let svg = d3.select(this._element).select("svg").select("g");
+
+    if (svg.empty()) {
+      svg = d3.select(this._element)
+        .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+    }
 
     let data = this._data,
       _x = this._x,
@@ -82,79 +88,112 @@ export class ColumnChart {
 
     //---------------------------------
     // add the Y gridlines
-    let grid = svg.append("g")
-        .attr("class", "grid")
-        .call(d3.axisLeft(y)
-            .tickSize(-width)
-            .tickFormat("")
-        )
-
-    svg.selectAll(".grid line")
-      .style("stroke", colours.eighteen.lightGrey);
-    svg.selectAll(".grid .domain")
-      .style("opacity", 0);
-
-    grid.style("opacity", 0)
-      .transition()
-      .delay(500)
-      .duration(500)
-      .style("opacity", 1);
+    svg.call(this.grid, width, y);
 
     //---------------------------------
     // append the rectangles for the bar chart
-    let bars = svg
-      .append("g")
-        .attr("transform", "translate(0, " + height + "), scale(1, -1)")
-      .selectAll(".bar")
-        .data(data)
+    let bars = svg.select(".bars").selectAll('.bar');
 
-    bars.enter()
-      .append("rect")
-        .attr("class", "bar")
-        .attr("x", (d) => x(_x(d)))
+    if (bars.empty()) {
+      bars = svg
+        .append("g")
+          .attr("class", "bars")
+          .attr("transform", "translate(0, " + height + "), scale(1, -1)")
+        .selectAll(".bar");
+    }
+    bars = bars.data(data);
+
+    let setupBarAttributes = (selection) => {
+      selection.attr("x", (d) => x(_x(d)))
         .attr("y", 0)
         .attr("width", x.bandwidth())
         .attr("height", 0)
+    }
+
+    bars   // set x and width for existing bars, animating them.
+      .interrupt("bar:move")
+      .transition("bar:move")
+      .call(setupBarAttributes);
+
+    bars.enter()
+      .append("rect")                      // Create the geometry
+        .attr("class", "bar")
+        .call(setupBarAttributes)          // Set x and width for new bars, not animating.
         .style("fill", colours.eighteen.midGrey)
-        .on("mouseover", (d, i, nodes) => {
+        .on("mouseover", (d, i, nodes) => { // Darken the bar on mouse over
           d3.select(nodes[i])
             .interrupt("hover:colour")
             .transition("hover:colour")
             .duration(400)
             .style("fill", d3.hsl(colours.eighteen.midGrey).darker())
         })
-        .on("mouseout", (d, i, nodes) => {
+        .on("mouseout", (d, i, nodes) => { // bar is regular colour on mouse out.
           d3.select(nodes[i])
             .interrupt("hover:colour")
             .transition("hover:colour")
             .duration(400)
             .style("fill", colours.eighteen.midGrey);
         })
-      .transition()
-        .delay((d,i) => i * 100 )
-        .attr("height", (d) => height - y(_y(d)));
+        .merge(bars)                // For both enter and update selections.
+        .interrupt("bar:growth")
+        .transition("bar:growth")   // Animate the bars to their new position.
+          .delay((d,i) => i * 100 )
+          .attr("height", (d) => height - y(_y(d)));
 
-    // add the x Axis
-    svg.append("g")
-        .attr("class", "x-axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
-        .style("opacity", 0)
-        .transition()
-        .duration(1000)
-        .style("opacity", 1);
+    bars.exit().remove();
 
-    // add the y Axis
-    svg.append("g")
-        .attr("class", "y-axis")
-        .call(d3.axisLeft(y).tickSize(0).tickPadding(10))
-        .style("opacity", 0)
-        .transition()
-        .duration(1000)
-        .style("opacity", 1);
+    // axes
+    svg.call(this.xaxis, height, x);
+    svg.call(this.yaxis, y);
 
     svg.selectAll("text")
       .style("fill", colours.eighteen.darkGrey);
 
+  }
+
+  grid(selection, width, yscale) {
+    selection.select(".grid").remove();
+
+    let grid = selection.append("g")
+        .attr("class", "grid")
+        .call(d3.axisLeft(yscale)
+            .tickSize(-width)
+            .tickFormat("")
+        )
+
+    grid.selectAll("line")
+      .style("stroke", colours.eighteen.lightGrey);
+    grid.selectAll(".domain").remove()
+
+    grid
+      .lower()
+      .style("opacity", 0)
+      .transition()
+      .delay(500)
+      .duration(500)
+      .style("opacity", 1);
+  }
+
+  xaxis(selection, height, xscale) {
+    selection.select(".x-axis").remove();
+    selection.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xscale))
+        .style("opacity", 0)
+        .transition()
+        .duration(1000)
+        .style("opacity", 1);
+  }
+
+  yaxis(selection, yscale) {
+    selection.select(".y-axis").remove();
+    selection.append("g")
+        .attr("class", "y-axis")
+        .call(d3.axisLeft(yscale).tickSize(0).tickPadding(10))
+        .style("opacity", 0)
+        .transition()
+        .duration(1000)
+        .style("opacity", 1);
   }
 }
